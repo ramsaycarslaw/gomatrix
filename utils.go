245dc249ef7dcf797a64ed4d787414396beb6af9
ramsaycarslaw <50293558+ramsaycarslaw@users.noreply.github.com>
2019-05-05 17:05:59 +0100

    Add files via upload

--- utils.go
+++ utils.go
@@ -10,3 +10,20 @@ func raiseError(s string) (err error) {
 	err = fmt.Errorf(s)
 	return
 }
+
+func PrintMat(mat Matrix) {
+	//rows := int(mat[0])
+	colums := int(mat[1])
+	matrix := [][]float64{}
+	row := []float64{}
+	for _, v := range mat[2:] {
+		row = append(row, v)
+		if len(row) == colums {
+			matrix = append(matrix, row)
+			row = []float64{}
+		}
+	}
+	for _, v := range matrix {
+		fmt.Println(v)
+	}
+}
