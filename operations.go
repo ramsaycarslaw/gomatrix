2a2ed568c89f652bb48a0192db1328c4a8ea9add
ramsaycarslaw <50293558+ramsaycarslaw@users.noreply.github.com>
2019-07-06 20:14:54 +0100

    Added dimensions and Add functions
    Can now add matrices and check their dimensions

--- operations.go
+++ operations.go
@@ -1,6 +1,8 @@
 package gomatrix
 
-import "fmt"
+import (
+	"fmt"
+)
 
 //Dot multiplys two matrices
 func Dot(mat1 Matrix, mat2 Matrix) (result Matrix, err error) {
@@ -23,6 +25,20 @@ func DotProduct(row Row, colum Row) (product float64, err error) {
 	}
 	for _, v := range products {
 		product = product + v
+	}
+	return
+}
+
+//Add adds two matrices of the same dimensions
+func Add(mat1 Matrix, mat2 Matrix) (result Matrix, err error) {
+	if mat1[0] != mat2[0] && mat1[1] != mat2[1] {
+		err = raiseError(fmt.Sprintf("Matrices not aligned: cannot perform addition"))
+		return
+	}
+	matrix1 := mat1[2:]
+	matrix2 := mat2[2:]
+	for i := range matrix1 {
+		result[i] = matrix1[i] + matrix2[i]
 	}
 	return
 }
--- utils.go
+++ utils.go
@@ -2,6 +2,7 @@ package gomatrix
 
 import "fmt"
 
+//IndexBuf gets iteration size of matrix
 func (matrix Matrix) IndexBuf(row, col int) int {
 	return row*int(matrix[1]) + col + 2
 }
@@ -11,11 +12,20 @@ func raiseError(s string) (err error) {
 	return
 }
 
+//PrintMat gets nested form and prints each slice on a newline
 func PrintMat(mat Matrix) {
+	matrix := NestedForm(mat)
+	for _, v := range matrix {
+		fmt.Println(v)
+	}
+}
+
+//NestedForm returns type matrix as a nested Row type
+func NestedForm(mat Matrix) []Row {
 	//rows := int(mat[0])
 	colums := int(mat[1])
-	matrix := [][]float64{}
-	row := []float64{}
+	matrix := []Row{}
+	var row Row
 	for _, v := range mat[2:] {
 		row = append(row, v)
 		if len(row) == colums {
@@ -23,7 +33,13 @@ func PrintMat(mat Matrix) {
 			row = []float64{}
 		}
 	}
-	for _, v := range matrix {
-		fmt.Println(v)
-	}
+	return matrix
+}
+
+//Dims returns the dimensions of a given type Matrix
+func Dims(mat Matrix) []int {
+	var dimensions []int
+	dimensions[0] = int(mat[0])
+	dimensions[1] = int(mat[1])
+	return dimensions
 }
