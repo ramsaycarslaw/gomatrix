e858a82d907cc947b31b01608c4040d2cdf91c50
ramsaycarslaw <50293558+ramsaycarslaw@users.noreply.github.com>
2019-10-02 14:08:49 +0100

    Changed struct style

--- matrix.go
+++ matrix.go
@@ -1,61 +1,46 @@
 package gomatrix
 
 //Matrix is the default type in gomatrix
-type Matrix []float64
-
-/*
-//Row is used for construction
-type Row []float64
-
-//Columns is a list of rows
-type Columns []Row*/
+type Matrix struct {
+	Rows uint // ?Maybe not needed
+	Cols uint
+	Data []float64
+}
 
 //GenerateMatrix creates a blank matrix
-func GenerateMatrix(rows, cols int) (mat Matrix) {
-	mat = make(Matrix, rows*cols+2)
-	mat[0] = float64(rows)
-	mat[1] = float64(cols)
-
+func GenerateMatrix(rows, cols uint) (matrix Matrix) {
+	data := make([]float64, rows*cols)
+	matrix.Data = data
+	matrix.Rows = rows
+	matrix.Cols = cols
 	return
 }
 
 //FilledMatrix creates a new matrix on a single thread
-func FilledMatrix(rows, cols int, data []float64) (matrix Matrix, err error) {
+func FilledMatrix(rows, cols uint, data []float64) (matrix Matrix, err error) {
 	if rows == 0 || cols == 0 {
 		err = raiseError("Cannot make empty matrix")
 		return
-	} else if rows*cols != len(data) {
+	} else if rows*cols != uint(len(data)) {
 		err = raiseError("Incorrect number of elements")
 	}
 	matrix = GenerateMatrix(rows, cols)
-	for i := range data {
-		matrix[i+2] = data[i]
-	}
-	return
-}
-
-//Zeros creates a matrix and fills it with zeros
-func Zeros(rows, cols int) (mat Matrix) {
-	mat = make(Matrix, rows*cols+2)
-	var zero float64
-	for i := range mat {
-		mat[i] = zero
-	}
-	mat[0] = float64(rows)
-	mat[1] = float64(cols)
+	matrix.Data = data
 
 	return
 }
 
 //RandFill creates and fills a matrix with random integers
-func RandFill(rows, cols, min, max int) (mat Matrix) {
-	mat = make(Matrix, rows*cols+2)
-	for i := range mat {
+func RandFill(rows, cols uint, min, max int) (mat Matrix) {
+	mat = GenerateMatrix(rows, cols)
+	for i := range mat.Data {
 		random := randInt(min, max)
-		mat[i] = float64(random)
+		mat.Data[i] = float64(random)
 	}
-	mat[0] = float64(rows)
-	mat[1] = float64(cols)
-
 	return
+}
+
+//Delete removes all items from a matrix
+func (matrix Matrix) Delete() {
+	matrix.Data = nil
 }
